eaeff5057cb3b1084f32984a3efd7a02198842b3
sarhatabaot <sarhatabaot1@gmail.com>
2018-12-16 18:39:55 +0200

    Added position class

---
+++ src/main/java/com/koletar/jj/mineresetlite/Position.java
@@ -0,0 +1,61 @@
+package com.koletar.jj.mineresetlite;
+
+public class Position {
+    private int x,y,z;
+    private int yaw;
+    private int pitch;
+
+    public Position(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Position(int x, int y, int z, int yaw, int pitch) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public int getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(int yaw) {
+        this.yaw = yaw;
+    }
+
+    public int getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(int pitch) {
+        this.pitch = pitch;
+    }
+}
