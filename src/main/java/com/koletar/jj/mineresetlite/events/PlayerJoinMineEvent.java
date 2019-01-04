4d2000647b74a236871434f268ae7e35a0779b1d
sarhatabaot <sarhatabaot1@gmail.com>
2019-01-04 21:46:36 +0200

    Added Empty PlayerJoinMineEvent
    for future use - with potion effects

---
+++ src/main/java/com/koletar/jj/mineresetlite/events/PlayerJoinMineEvent.java
@@ -0,0 +1,11 @@
+package com.koletar.jj.mineresetlite.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerJoinMineEvent extends Event {
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
